000c1ff2223e50a3478f0f0fe4f213da295f2519
Shannon Heylmun <shannonheylmun@gmail.com>
2024-07-19 20:13:17 -0400

    Try getting the output right

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withNextra = require("nextra")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+	output: "export",
 	trailingSlash: true,
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 	images: {
